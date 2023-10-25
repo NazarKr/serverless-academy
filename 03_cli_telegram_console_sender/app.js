fc557703480b76a8937dc8840a68c45020d8f11e
Nazar Kruzhylko <nazarok1990@gmail.com>
2023-10-25 22:05:39 +0300

    add code

---
+++ 03_cli_telegram_console_sender/app.js
@@ -0,0 +1,24 @@
+const program = require("commander");
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+
+const TOKEN = process.env.TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+program
+  .command("m <message>")
+  .description("Send a message to the Telegram bot.")
+  .action((message) => {
+    bot.sendMessage(CHAT_ID, message);
+  });
+
+program
+  .command("p <path>")
+  .description("Send a photo to the Telegram bot.")
+  .action((path) => {
+    bot.sendPhoto(CHAT_ID, path);
+  });
+
+program.parse(process.argv);
