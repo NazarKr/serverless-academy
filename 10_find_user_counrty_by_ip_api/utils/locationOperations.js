92998ec033cabbb3c1c81abf308a846e00f5c964
Nazar Kruzhylko <nazarok1990@gmail.com>
2023-11-08 15:27:44 +0200

    fix path

--- 10_find_user_counrty_by_ip_api/utils/locationOperations.js
+++ 10_find_user_counrty_by_ip_api/utils/locationOperations.js
@@ -1,19 +1,16 @@
 const csv = require("csv-parser");
 const ip = require("ip");
-const path = require("node:path");
+const path = require("path");
 const { createReadStream } = require("node:fs");
 
-
-const ipRecordsPath = path.resolve(
-  "C:\\Users\\User\\Documents\\GitHub\\serverless-academy\\10_find_user_counrty_by_ip_api\\data\\IP2LOCATION-LITE-DB1.csv"
-);
+const ipLocationPath = path.resolve("data/IP2LOCATION-LITE-DB1.csv");
 
 async function findLocationByIp(userIp) {
   const results = [];
   const parsedIp = ip.toLong(userIp);
 
   const getLocationFromList = new Promise((resolve, reject) => {
-    createReadStream(ipRecordsPath)
+    createReadStream(ipLocationPath)
       .pipe(csv())
       .on("error", (error) => {
         reject(error);
